ad282aeccb2ccbfa19207880207565053447b874
Kyle Hill <kylehill@gmail.com>
2015-04-30 16:07:56 -0400

    Sails-enabled, saves to disk

---
+++ api/controllers/DataController.js
@@ -0,0 +1,36 @@
+var renderHome = function(res) {
+  res.render("homepage", {
+    text: "/*\n* Meet LumberJack!\n* It's tool for anyone that's lecturing on\n*   JS fundamentals to new developers.\n* Think of it as a more illustrative console.log().\n*\n* Call the function log() with any parameter \n*   to log it out to the right side w/ line number.\n*/\nlog(\"hello, world!\");\n\nfunction greeter(person) {\n  log(person);\n  return \"Hi, \" + person;\n}\n\nvar salutation = greeter(\"LumberJack\");\nlog(salutation);"
+  })
+}
+
+module.exports = {
+
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false
+  },
+	
+  save: function(req, res) {
+    Data.add(req.body, function(err, s){
+      res.json({ slug: s.slug })
+    })
+  },
+
+  read: function(req, res) {
+    Data.findOne({ slug: req.params.slug }).exec(function(err, data){
+      if (!data) {
+        renderHome()
+        return
+      }
+      res.render("homepage", { text: data.text })
+    })
+  },
+
+  home: function(req, res) {
+    renderHome()
+  }
+
+};
+
---
+++ api/models/Data.js
@@ -0,0 +1,23 @@
+/**
+* Data.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+var shortid = require("shortid")
+
+module.exports = {
+
+  attributes: {
+
+  },
+
+  add: function(data, callback) {
+    Data.create(data, function(err, data){
+      data.slug = shortid.generate()
+      data.save(callback)
+    })
+  }
+};
+
